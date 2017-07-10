8b187bdd40e1d315b4f70ebea972b1486d82b569
Florin Stancu <stancu.florin23@gmail.com>
2017-07-10 13:21:03 +0000

    final version

--- samsung-smart-tv-remote.py
+++ samsung-smart-tv-remote.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import configparser, pika, json, samsungctl
+import configparser, pika, json, time, samsungctl
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -10,7 +10,7 @@ params.socket_timeout = 5
 connection = pika.BlockingConnection(params) # Connect to CloudAMQP
 channel = connection.channel() # start a channel
 
-REMOTE = {
+config_remote = {
     "name": config['SamsungSmartTV']['name'],
     "description": config['SamsungSmartTV']['description'],
     "id": "",
@@ -20,32 +20,35 @@ REMOTE = {
     "timeout": 0
 }
 
-def change_channel(channel, remote):
-	for digit in channel:
-		remote.control("KEY_" + digit)
-	remote.control("KEY_ENTER")
-	print 'The channel was changed to ', channel
+def change_channel(channel):
+	with samsungctl.Remote(config_remote) as remote:
+		for digit in channel:
+			print "working on", digit
+			remote.control("KEY_" + digit)
+			time.sleep(0.5)
+		remote.control("KEY_ENTER")
+		print "The channel was changed to", channel
 
-def turn_off_tv(remote):
-    print 'The TV is shutting down'
-    remote.control("KEY_POWER")
+def turn_off_tv():
+	with samsungctl.Remote(config_remote) as remote:
+		print "The TV is shutting down"
+		remote.control("KEY_POWER")
 	
 # create a function which is called on incoming messages
 def callback(ch, method, properties, body):
 	message = json.loads(body)
-	remote = samsungctl.Remote(REMOTE)
 	
 	if message['command'] == "CHANGE_CHANNEL":
-		change_channel(message['value'], remote)
+		change_channel(message['value'])
 	elif message['command'] == "TURN_OFF":
-		turn_off_tv(remote)
+		turn_off_tv()
 	else:
-		print 'There is no custom command implemented for ', message['command']
+		print "There is no custom command implemented for", message['command']
 		
 # set up subscription on the queue
 channel.basic_consume(callback, queue=config['CloudAMQP']['queue'], no_ack=True)
 
-print "Waiting for commands"
+#print "Waiting for commands"
 channel.start_consuming() # start consuming (blocks)
 
 connection.close()
